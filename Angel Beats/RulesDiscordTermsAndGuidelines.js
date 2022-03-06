375d00a1e5fc7a210f4ca2d7878fd26a54eeff4d
LykaonBotkin <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 14:34:24 -0800

    Create RulesDiscordTermsAndGuidelines.js

---
+++ Angel Beats/RulesDiscordTermsAndGuidelines.js
@@ -0,0 +1,6 @@
+{
+    "color":12675939,
+    "type":"rich",
+    "description":"***The rules below are laid before you, and may change at any time. It is your responsibility to keep up-to-date with the rules and abide by the [Discord Terms of Service](https:\/\/discord.com\/terms) and [Community Guidelines](https:\/\/discord.com\/guidelines).***",
+    "title":"__Discord Terms and Guidelines__"
+}
