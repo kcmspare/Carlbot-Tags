df75cfa447dde6ff0d155cea41c04204937e8098
Lykaon <65002282+LykaonBotkin@users.noreply.github.com>
2022-03-06 18:09:50 -0800

    Delete Angel Beats/AngelBeats_Information directory

--- Angel Beats/AngelBeats_Information/Welcome/WelcomeDescription.js
+++
@@ -1,6 +0,0 @@
-{
-    "color":16251647,
-    "type":"rich",
-    "description":"We are a community focused around the Angel Beats! anime, visual novel, and mangas, as well as discussing common interests! We hope that you enjoy your time here and meet a lot of cool people!",
-    "title":"Welcome to the Angel Beats server!"
-}
